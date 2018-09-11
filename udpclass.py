c098ee7d623dcd35f2f24795297e31439cac2d8e
km2457 <31201028+km2457@users.noreply.github.com>
2018-09-11 19:25:00 +0800

    Update udpclass.py

--- udpclass.py
+++ udpclass.py
@@ -655,6 +655,7 @@ class udpclass:
                                   answer=self.create_answer(select_header, self.data_pick_cmdid(other_data)))
                 elif '0x5' in data_cmdid_array:
                     print('config')
+                    print(binascii.hexlify(data))
                     #print(other_data)
                     #print(data_side)
                     print(self.create_answer(select_header, self.data_pick_cmdid(data_side)))
